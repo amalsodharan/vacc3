ffed5d946809002b1b533eafe03628399ba3cdb9
amalsodharan <138609327+amalsodharan@users.noreply.github.com>
2023-09-21 11:58:13 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"> </script>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="stylesheet" href="search.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="top">
+                <div class="row">
+                    <div class="col">
+                        <h2>e-vaccines</h2>
+                    </div>
+                    <div class="col-5">
+                        <ul>
+                            <li><a href="index.php">HOME</a></li>
+                            <li><a href="login.php">LOGIN</a></li>
+                            <li><a href="#">ABOUT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header><br><br><br><br><br>
+    <div class="vacc">
+        <h2>Search your slot</h2>
+        <div class="age">
+            <div class="age-head">
+                <h4>Age</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="danger-outlined">
+                <label class="btn btn-outline-danger" for="danger-outlined">12-17</label>
+                <input type="checkbox" class="btn-check" id="danger-outlined">
+                <label class="btn btn-outline-danger" for="danger-outlined">15+</label>
+            </div>
+    </div>
+</body>
+</html>
