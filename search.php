96e3a8ecb3730d9d802f5e3df08704fc732dbdba
amalsodharan <138609327+amalsodharan@users.noreply.github.com>
2023-09-24 14:33:17 +0530

    Add files via upload

--- search.php
+++ search.php
@@ -12,6 +12,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="stylesheet" href="search.css">
+    <link rel="stylesheet" href="search2.css">
 </head>
 <body>
     <header>
@@ -32,18 +33,106 @@
             </div>
         </nav>
     </header><br><br><br><br><br>
+    <form action="">
     <div class="vacc">
-        <h2>Search your slot</h2>
+        <h2 style="text-align: center;color: rgba(21, 98, 212, 0.89);font-size: 35px;">Search your slot</h2>
+        <center>
         <div class="age">
+            <hr>
             <div class="age-head">
                 <h4>Age</h4>
             </div>
             <div class="age-body">
-                <input type="checkbox" class="btn-check" id="danger-outlined">
-                <label class="btn btn-outline-danger" for="danger-outlined">12-17</label>
-                <input type="checkbox" class="btn-check" id="danger-outlined">
-                <label class="btn btn-outline-danger" for="danger-outlined">15+</label>
+                <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-outlined">12-17</label>
+                <input type="checkbox" class="btn-check" id="btn-check-2-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-2-outlined">18+</label>
+                <input type="checkbox" class="btn-check" id="btn-check-3-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-3-outlined">45+</label>
             </div>
-    </div>
+        </div>
+        <div class="age">
+            <hr>
+            <div class="age-head">
+                <h4>Dose</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="btn-check-4-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-4-outlined">Dose 1</label>
+                <input type="checkbox" class="btn-check" id="btn-check-5-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-5-outlined">Dose 2</label>
+            </div>
+        </div>
+        <div class="age">
+            <hr>
+            <div class="age-head">
+                <h4>Cost</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="btn-check-6-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-6-outlined">Free</label>
+            </div>
+        </div>
+        <div class="age">
+            <hr>
+            <div class="age-head">
+                <h4>Vaccine</h4>
+            </div>
+            <div class="age-body">
+                <input type="checkbox" class="btn-check" id="btn-check-7-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-7-outlined">Covishield</label>
+                <input type="checkbox" class="btn-check" id="btn-check-8-outlined" autocomplete="off">
+                <label class="btn btn-outline-primary btn-sm" for="btn-check-8-outlined">Covaxin</label>
+            </div>
+        </div>
+        </center><br><br>
+        <div class="overflow-auto" id="dat">
+        <div class="datee">
+            <ul>
+                <li><a href="#"><button class="dat-but">Mon,1 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,2 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,3 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,4 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,5 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,6 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,7 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,8 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,9 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,10 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,11 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,12 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,13 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,14 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,15 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,16 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,17 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,18 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,19 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,20 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,21 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,22 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,23 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,24 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Thu,25 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Fri,26 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sat,27 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Sun,28 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Mon,29 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Tue,30 jan</button></a></li>
+                <li><a href="#"><button class="dat-but">Wed,31 jan</button></a></li>
+            </ul>
+        </div>
+        </div><br><br>
+        <div class="subm">
+            <input type="submit" value="Check for Avialability" class="but"><br><br>
+        </div>
+    </form>
+        <div class="last">
+            <h5>For Grievances and Feedback :<br> evaccine@gmail.com<br><br>
+            Ph no : 0481-246861<br><br>
+            Privacy Policy | Terms of Service<br><br>
+            Copyright © 2021 e-vaccine. All Rights Reserved
+            </h5> 
+        </div>
 </body>
 </html>
